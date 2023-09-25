1672b5df21c52b216cc2d9272957d7a9bea81430
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-09-25 07:15:26 -0700

    Sorting algorithms & Big O

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
